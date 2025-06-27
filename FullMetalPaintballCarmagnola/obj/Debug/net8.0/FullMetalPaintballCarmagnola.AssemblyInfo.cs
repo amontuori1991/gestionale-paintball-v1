0fea6258b4f5f9457d9d958fd8ba87997f23e4d1
amontuori1991 <alexmontuori1991@gmail.com>
2025-06-27 09:28:04 +0200

    Fix Logo HOME Smartphone

--- FullMetalPaintballCarmagnola/obj/Debug/net8.0/FullMetalPaintballCarmagnola.AssemblyInfo.cs
+++ FullMetalPaintballCarmagnola/obj/Debug/net8.0/FullMetalPaintballCarmagnola.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52fb65d9a3a446eedb28f0acb343650421f1a315")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c7dc192849bbbdc4c621965cce6502dbdf38ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
