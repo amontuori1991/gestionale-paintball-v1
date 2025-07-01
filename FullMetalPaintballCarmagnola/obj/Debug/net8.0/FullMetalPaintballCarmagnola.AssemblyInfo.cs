56fd0203fa8ce7f330fc3696a85fd1562ff27631
amontuori1991 <alexmontuori1991@gmail.com>
2025-07-01 17:04:42 +0200

    Fix Caparre

--- FullMetalPaintballCarmagnola/obj/Debug/net8.0/FullMetalPaintballCarmagnola.AssemblyInfo.cs
+++ FullMetalPaintballCarmagnola/obj/Debug/net8.0/FullMetalPaintballCarmagnola.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0976dd30a333102bbee33157867579f623154b0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1f7193bb228703e5944ad1b06f080da8284278")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullMetalPaintballCarmagnola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
