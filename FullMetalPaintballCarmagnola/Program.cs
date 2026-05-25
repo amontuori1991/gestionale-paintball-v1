bb9fd372825b825e5b8f27869d6c5ea7b4d1787d
amontuori1991 <a.montuori@azerouno.it>
2026-05-25 12:15:33 +0200

    Calcolo saldo finale

--- FullMetalPaintballCarmagnola/Program.cs
+++ FullMetalPaintballCarmagnola/Program.cs
@@ -139,7 +139,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
