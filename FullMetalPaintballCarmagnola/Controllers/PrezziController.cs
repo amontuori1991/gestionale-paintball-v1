192a96279b9be1d111561be629866e229279a4cc
amontuori1991 <a.montuori@azerouno.it>
2026-06-26 10:51:37 +0200

    Fix Listini

--- FullMetalPaintballCarmagnola/Controllers/PrezziController.cs
+++ FullMetalPaintballCarmagnola/Controllers/PrezziController.cs
@@ -2,6 +2,7 @@ using Full_Metal_Paintball_Carmagnola.Models;
 using Full_Metal_Paintball_Carmagnola.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Full_Metal_Paintball_Carmagnola.Controllers
 {
@@ -9,16 +10,25 @@ namespace Full_Metal_Paintball_Carmagnola.Controllers
     public class PrezziController : Controller
     {
         private readonly PricingCatalogService _pricingCatalogService;
+        private readonly TesseramentoDbContext _dbContext;
 
-        public PrezziController(PricingCatalogService pricingCatalogService)
+        public PrezziController(
+            PricingCatalogService pricingCatalogService,
+            TesseramentoDbContext dbContext)
         {
             _pricingCatalogService = pricingCatalogService;
+            _dbContext = dbContext;
         }
 
         public async Task<IActionResult> Index()
         {
             var catalog = await _pricingCatalogService.GetCatalogAsync();
+            var previousListinoId = catalog.GetLegacyListino().Id;
+            var showPreviousListino = previousListinoId != catalog.CurrentListinoId
+                && await _dbContext.Partite.AnyAsync(p => !p.IsDeleted && p.Listino == previousListinoId);
+
             ViewBag.CanManagePricing = User.IsInRole("Admin");
+            ViewBag.ShowPreviousListino = showPreviousListino;
             return View(catalog);
         }
 
